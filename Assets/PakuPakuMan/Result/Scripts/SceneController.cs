f22d86090586b62edb319917d66b219b43eab0d5
manntera <contact@manntera.com>
2020-10-11 15:24:20 +0900

    [Add]リザルトを追加しました。

---
+++ Assets/PakuPakuMan/Result/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Result
+{
+    public class SceneController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Title");
+            }
+        }
+    }
+}
