199f00e85a7a5f28bb6e6431c122b5eba6eed1c3
manntera <contact@manntera.com>
2020-10-11 14:33:29 +0900

    [Add]タイトルを追加しました。

---
+++ Assets/PakuPakuMan/Title/Script/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Title
+{
+    public class SceneController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("GameMain");
+            }
+        }
+    }
+}
